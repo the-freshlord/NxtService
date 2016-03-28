534a84a2e7eabfa75b53be16462765804afc3dab
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-28 15:17:05 -0400

    Finished the storyboard for when the user starts the app. Added custom cocoa touch classes for the buttons and views to give them a material design.

---
+++ NxtService/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  NxtService
+//
+//  Created by Emanuel  Guerrero on 3/28/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//Material Design
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
