65b761330d5b242963f07bb1f83883eec6c378da
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-28 21:16:06 -0400

    Finished creating the storyboard for the login / signup screen.

---
+++ NxtService/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  NxtService
+//
+//  Created by Emanuel  Guerrero on 3/28/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
