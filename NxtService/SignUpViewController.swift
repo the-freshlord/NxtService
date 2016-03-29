c157d095236c4969a1678654bd9106e04cbbab95
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-29 19:08:45 -0400

    Finished the sign up story board for when a new service provider creates an account.

---
+++ NxtService/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  NxtService
+//
+//  Created by Emanuel  Guerrero on 3/28/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
