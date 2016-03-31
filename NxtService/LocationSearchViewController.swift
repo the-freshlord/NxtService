4b72530b247040087f6b3853273f69663e3de783
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-30 20:01:45 -0400

    Completed the story board for the search location table.

---
+++ NxtService/LocationSearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LocationSearchViewController.swift
+//  NxtService
+//
+//  Created by Emanuel  Guerrero on 3/29/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class LocationSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
