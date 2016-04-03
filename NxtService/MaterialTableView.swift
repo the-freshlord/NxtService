e330383f47b58272ff189d0d0d681547ffe88e95
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-04-03 15:38:09 -0400

    Added the story boards for the location finder.

---
+++ NxtService/MaterialTableView.swift
@@ -0,0 +1,19 @@
+//
+//  MaterialTableView.swift
+//  NxtService
+//
+//  Created by Emanuel  Guerrero on 3/30/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class MaterialTableView: UITableView {
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSizeMake(0.0, 2.0)
+    }
+}
---
+++ NxtService/ProfileMenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileMenuViewController.swift
+//  NxtService
+//
+//  Created by Emanuel  Guerrero on 3/30/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class ProfileMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
