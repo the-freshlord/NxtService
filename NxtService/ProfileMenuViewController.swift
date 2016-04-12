6c26d5c3b3a93fc00521ccffdeb15d42afc64ccc
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-04-11 21:03:00 -0400

    Added the boiler plate code need for the profile menu view controller.

--- NxtService/ProfileMenuViewController.swift
+++ NxtService/ProfileMenuViewController.swift
@@ -9,11 +9,57 @@
 import UIKit
 
 class ProfileMenuViewController: UIViewController {
+    @IBOutlet weak var profileImageView: UIImageView!
+    
     var account: Account!
     var provider: Provider!
+    var imagePickerController: UIImagePickerController!
+    var imageSelect = false
     
     // MARK: - Navigation
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        profileImageView.layer.borderColor = UIColor(red: 220.0 / 255.0, green: 217.0 / 255.0, blue: 222.0 / 255.0, alpha: 1.0).CGColor
+        profileImageView.layer.borderWidth = 1.5
+        profileImageView.layer.cornerRadius = profileImageView.frame.size.width / 2
+        profileImageView.clipsToBounds = true
+        
+        imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+    }
+    
+    // MARK: - Events
+    @IBAction func cameraImageTapped(sender: UIButton) {
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func deleteButtonTapped(sender: MaterialButton) {
+    }
+    
+    // MARK: Helper methods
+    @IBAction func editInfoLabelTapped(sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func editCredentialsLabelTapped(sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func editServicesLabelTapped(sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func editLocationLabelTapped(sender: UITapGestureRecognizer) {
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate, UINavigationControllerDelegate
+extension ProfileMenuViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePickerController.dismissViewControllerAnimated(true, completion: nil)
+        profileImageView.image = image
+        imageSelect = true
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
     }
 }
