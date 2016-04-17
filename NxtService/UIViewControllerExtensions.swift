0b2032d0fcf99227e960b48dda025512ed03fafd
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-04-16 21:15:22 -0400

    Created a extension for the UIViewController so that we can modulize the code.

---
+++ NxtService/UIViewControllerExtensions.swift
@@ -0,0 +1,55 @@
+//
+//  UIViewExtensions.swift
+//  NxtService
+//
+//  Created by Emanuel  Guerrero on 4/13/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func showErrorAlert(title: String, message: String) {
+        // Check if an alert controller is already being presented
+        if self.presentedViewController == nil {
+            createNewAlertViewController(title, message: message)
+        } else {
+            // The alert controller is already presented or there is another view controller being presented
+            let thePresentedViewController: UIViewController? = self.presentedViewController as UIViewController?
+            
+            if thePresentedViewController != nil {
+                
+                // Check if the presented controller is an alert view controller
+                if let presentedAlertViewController: UIAlertController = thePresentedViewController as? UIAlertController {
+                    
+                    // Do nothing since the alert controller is already on screen
+                    print(presentedAlertViewController)
+                } else {
+                    // Another view controller presented, so use thePresentedViewController
+                    let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+                    let action = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+                    alert.addAction(action)
+                    thePresentedViewController?.presentViewController(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    func createNewAlertViewController(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        alert.addAction(action)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+
+    func startSpinning(indicatorView: UIView, activityIndicatorView: UIActivityIndicatorView) {
+        indicatorView.hidden = false
+        activityIndicatorView.startAnimating()
+    }
+    
+    func stopSpinning(indicatorView: UIView, activityIndicatorView: UIActivityIndicatorView) {
+        indicatorView.hidden = true
+        activityIndicatorView.stopAnimating()
+    }
+}
