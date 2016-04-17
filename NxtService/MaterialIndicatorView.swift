b9028ea57e7c1107f2cbaa2d39d39be41cd33d10
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-04-16 21:13:46 -0400

    Added a cocoa touch class for a custom view that contains an activity indicator view.

---
+++ NxtService/MaterialIndicatorView.swift
@@ -0,0 +1,19 @@
+//
+//  MaterialIndicatorView.swift
+//  NxtService
+//
+//  Created by Emanuel  Guerrero on 4/13/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class MaterialIndicatorView: UIView {
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+        layer.shadowColor = UIColor(red: MaterialDesign.SHADOW_COLOR, green: MaterialDesign.SHADOW_COLOR, blue: MaterialDesign.SHADOW_COLOR, alpha: 0.5).CGColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSizeMake(0.0, 2.0)
+    }
+}
