1e1a28bfc01e82bdb10487bbf0e8801bea0d1695
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-04-22 21:05:08 -0400

    Added a view controller for the search menu story board.

---
+++ NxtService/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  NxtService
+//
+//  Created by Emanuel  Guerrero on 4/22/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    // MARK: - Navigation
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
